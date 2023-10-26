88a47d002952c07a43cb916bfe9a47eed2573f78
Yitbarek-Wolde <yitbarek.ayni@gmail.com>
2023-10-26 08:39:54 -0500

    login signup pages updated, home page updated

--- home.js
+++ home.js
@@ -1,56 +1,77 @@
-function PerviousPicture(){
+function PerviousPicture() {
     let picChangeElements = document.querySelectorAll('.PicChange');
 
     for (let i = 0; i < picChangeElements.length; i++) {
         let element = picChangeElements[i];
-        
-        
-        element.addEventListener('click', function() {
-            
+
+
+        element.addEventListener('click', function () {
+
             let parentDiv = this.parentElement;
             let ImageContainer = parentDiv.previousElementSibling;
-            let OneCheck = ImageContainer.src.search('1');
-            let TwoCheck = ImageContainer.src.search('2');
-            let ThreeCheck = ImageContainer.src.search('3');
+            let Checking = ImageContainer.getAttribute('name');
 
-            if(OneCheck !== -1)
-                ImageContainer.src.replace('1','3')
-            else if(TwoCheck !== -1)
-                ImageContainer.src.replace('2','1')
-            else if(ThreeCheck !== -1)
-                ImageContainer.src.replace('3','2')
+            if (Checking.indexOf('1') !== -1) {
+                console.log(Checking)
+                let replaceText = Checking.replace('1', '3');
+                ImageContainer.name = replaceText;
+                ImageContainer.src = replaceText;
+            }
+            else if (Checking.indexOf('2') !== -1) {
+                console.log(Checking)
+                let replaceText = Checking.replace('2', '1');
+                ImageContainer.name = replaceText;
+                ImageContainer.src = replaceText;
+            }
+            else if (Checking.indexOf('3') !== -1) {
+                console.log(Checking)
+                let replaceText = Checking.replace('3', '2');
+                ImageContainer.name = replaceText;
+                ImageContainer.src = replaceText;
+            }
             else
                 console.log('Error')
-        
+
         })
+    }
 }
+
+function NextPicture() {
+    let picChangeElements = document.querySelectorAll('.PicChangeN');
+
+    for (let i = 0; i < picChangeElements.length; i++) {
+        let element = picChangeElements[i];
+
+
+        element.addEventListener('click', function () {
+
+            let parentDiv = this.parentElement;
+            let ImageContainer = parentDiv.previousElementSibling;
+            
+           
+            let Checking = ImageContainer.getAttribute('name');
+
+            if (Checking.indexOf('1') !== -1) {
+                console.log(Checking)
+                let replaceText = Checking.replace('1', '2');
+                ImageContainer.name = replaceText;
+                ImageContainer.src = replaceText;
+            }
+            else if (Checking.indexOf('2') !== -1) {
+                console.log(Checking)
+                let replaceText = Checking.replace('2', '3');
+                ImageContainer.name = replaceText;
+                ImageContainer.src = replaceText;
+            }
+            else if (Checking.indexOf('3') !== -1) {
+                console.log(Checking)
+                let replaceText = Checking.replace('3', '1');
+                ImageContainer.name = replaceText;
+                ImageContainer.src = replaceText;
+            }
+            else
+                console.log('Error')
+        })
     }
 
-function NextPicture(){
-    let picChangeElements = document.querySelectorAll('.PicChange');
-
-    for (let i = 0; i < picChangeElements.length; i++) {
-        let element = picChangeElements[i];
-        
-        
-        element.addEventListener('click', function() {
-            
-            let parentDiv = this.parentElement;
-            let ImageContainer = parentDiv.previousElementSibling;
-            let OneCheck = ImageContainer.src.search('1');
-            let TwoCheck = ImageContainer.src.search('2');
-            let ThreeCheck = ImageContainer.src.search('3');
-
-            if(OneCheck !== -1)
-                ImageContainer.src.replace('1','2')
-            else if(TwoCheck !== -1)
-                ImageContainer.src.replace('2','3')
-            else if(ThreeCheck !== -1)
-                ImageContainer.src.replace('3','1')
-            else
-                console.log('Error')
-        })
 }
-
-}
-
--- jsPart.js
+++ jsPart.js
@@ -1,31 +1,53 @@
-function Loginsubmit(){
+function Loginsubmit() {
 
-    let firstinput=document.getElementById("inputEmail")
+  let firstinput = document.getElementById("inputEmail")
 
-    let secondinput=document.getElementById("inputPassword")
+  let secondinput = document.getElementById("inputPassword")
 
-    let forminput=document.querySelector('form')
+  let forminput = document.querySelector('form')
 
-    if(firstinput.value=="sem" && secondinput.value=="pass11"){
+  if (firstinput.value == "sem" && secondinput.value == "pass11") {
 
-        window.location.href ="home.html"
-    }
+    window.location.href = "home.html"
+  }
 
-    else {
-        alert("Access Denied")
-    }
+  else {
+    alert("Access Denied")
+  }
 
 }
 
 function NavFuction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  function SignUp(){
-    //added code later
+function SignUp() {
+  //added code later
+  let firstname = document.getElementById("inputfirstname").value
+  let secondname = document.getElementById("inputsecondname").value
+  let passwordOne = document.getElementById("inputPasswordOne").value
+  let passwordTwo = document.getElementById("inputPasswordTwo").value
+  let EmailOne = document.getElementById("inputEmailOne").value
+  if (firstname.length < 1)
+    document.getElementById("inputfirstname").style.border = '3px solid red'
+  if (secondname.length < 1)
+    document.getElementById("inputsecondname").style.border = '3px solid red'
+  if (EmailOne.length < 1)
+    document.getElementById("inputEmailOne").style.border = '3px solid red'
+  if (passwordOne.length < 6) {
+    document.getElementById("inputPasswordOne").style.border = '3px solid red'
+    alert('Passowrd must be 6 character long')
   }
+  if (passwordOne !== passwordTwo) {
+    document.getElementById("inputPasswordOne").style.border = '3px solid red'
+    document.getElementById("inputPasswordTwo").style.border = '3px solid red'
+    alert('Passwords do not match!')
+  }
+  if ((firstname.length > 1 && secondname.length > 1) && (passwordOne == passwordTwo) && (EmailOne.length > 1))
+    window.location.href = "home.html"
+}
