69ae7d3824af4ab02c2e1a91bd3c111ed4d16fe3
semhal berhe <semhalberhe18@gmail.com>
2023-10-26 15:59:58 -0500

    media query added

--- jsPart.js
+++ jsPart.js
@@ -1,27 +1,40 @@
+//email and password from the sign up form
+
+let email1=document.getElementById('inputEmail')
+let pw=document.getElementById('inputPassword')
+
+//storing input from sign up page
+
+function Store(){
+ localStorage.setItem('email1',email1.value)
+ localStorage.setItem('pw',pw.value)
+}
+
 function Loginsubmit(){
+//store data from sign up page
+storedemail=document.localStorage.getItem("email1")
 
-    let firstinput=document.getElementById("inputEmail")
+storedpw=document.localStorage.getItem("pw")
+//entered data from the log in form
+  let inputEmail=document.getElementById("inputEmail")
+  let inputPassword=document.getElementById("inputPassword")
+//check if stored data from sign up page is equal to data form log in page
+  if(inputEmail.value==storedemail || inputPassword.value==storedpw){
 
-    let secondinput=document.getElementById("inputPassword")
+      window.location.href ="login.html"
+  }
 
-    let forminput=document.querySelector('form')
-
-    if(firstinput.value=="sem" && secondinput.value=="pass11"){
-
-        window.location.href ="home.html"
-    }
-
-    else {
-        alert("Access Denied")
-    }
+  else {
+      alert("Access Denied")
+  }
 
 }
 
-function NavFuction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+// function NavFuction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
